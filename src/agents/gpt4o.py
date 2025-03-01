0113403dfa02c06a27612daa4f20d4ff9e7cbce3
Prem Acharya <102874190+prem-acharya@users.noreply.github.com>
2025-03-01 14:07:59 +0530

    feat(time_tool): add Current Time Tool for fetching current time by city

--- src/agents/gpt4o.py
+++ src/agents/gpt4o.py
@@ -2,22 +2,23 @@ from langchain.agents import initialize_agent, AgentType
 from langchain_openai import AzureChatOpenAI
 from src.tools.weather.weather_tool import WeatherTool
 from src.tools.aboutme_tool import AboutMeTool
+from src.tools.time_tool import CurrentTimeTool
 import os
 
-# print(os.getenv("AZURE_OPENAI_API_KEY"))
+print(os.getenv("AZURE_OPENAI_API_KEY"))
 
 class GitHubGPTAgent:
 
     def __init__(self):
         self.llm = AzureChatOpenAI(
-            openai_api_key= os.getenv("AZURE_OPENAI_API_KEY"),
+            openai_api_key= "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",
             azure_endpoint= os.getenv("AZURE_OPENAI_ENDPOINT"),
             deployment_name= "gpt-4o",
             model_name="gpt-4o",
             api_version="2024-02-15-preview",
             temperature=0.7)
 
-        self.tools = [WeatherTool(), AboutMeTool()]
+        self.tools = [WeatherTool(), AboutMeTool(), CurrentTimeTool()]
 
         self.agent = initialize_agent(
             tools=self.tools,
@@ -32,17 +33,18 @@ class GitHubGPTAgent:
 
             # Initialize used_tools list
             used_tools = []
-
-            # Check if weather information is present in the response
+                
             if "temperature" in result.lower() or "weather" in result.lower():
-                used_tools.append("weather_tool")
-            if "full-stack developer" in result.lower():
+                used_tools.append("weather")
+            if "software developer" in result.lower():
                 used_tools.append("about_me")
+            if "time" in result.lower():
+                used_tools.append("current_time")
 
             return {
                 "response": result,
                 "error": None,
-                "agent": ", ".join(used_tools) if used_tools else "none",
+                "agent": ", ".join(used_tools),
                 "model": self.llm.deployment_name
             }
         except Exception as e:
